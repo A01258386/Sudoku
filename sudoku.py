286cef462206e78da60e84ea5c78ea58d142d38a
Gokce Gokmen <51439620+A01258386@users.noreply.github.com>
2021-11-17 15:18:33 -0800

    Sudoku starter code

---
+++ sudoku.py
@@ -0,0 +1,95 @@
+#Sudoku and A* solver
+import pygame
+
+
+
+class Grid:
+    board = [
+        [7, 8, 0, 4, 0, 0, 1, 2, 0],
+        [6, 0, 0, 0, 7, 5, 0, 0, 9],
+        [0, 0, 0, 6, 0, 1, 0, 7, 8],
+        [0, 0, 7, 0, 4, 0, 2, 6, 0],
+        [0, 0, 1, 0, 5, 0, 9, 3, 0],
+        [9, 0, 4, 0, 6, 0, 0, 0, 5],
+        [0, 7, 0, 3, 0, 0, 0, 1, 2],
+        [1, 2, 0, 0, 0, 7, 4, 0, 0],
+        [0, 4, 9, 2, 0, 6, 0, 0, 7]
+    ]
+
+    def __init__():
+       pass
+
+    def sketch(self, val):
+        pass
+
+    def draw(self):
+        # Draw Grid Lines
+  
+        # Draw Cubes
+
+    def update_model(self):
+        pass
+
+    def is_finished(self):
+        pass
+
+    def solve(self):
+        pass
+
+
+class Cube:
+    rows = 9
+    cols = 9
+
+    def __init__():
+        pass
+
+    def draw(self, win):
+        pass
+
+    def draw_change(self, win, g=True):
+        
+        if g:
+            pygame.draw.rect(win, (0, 255, 0), (x, y, gap, gap), 3)
+        else:
+            pygame.draw.rect(win, (255, 0, 0), (x, y, gap, gap), 3)
+
+
+def redraw_window(win):
+
+    # Draw grid and board
+    board.draw()
+
+
+def main():
+    win = pygame.display
+    pygame.display.set_caption("Sudoku")
+    board = Grid()
+    while run:
+
+        for event in pygame.event.get():
+            if event.type == 
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_1:
+                    key = 1
+                if event.key == pygame.K_2:
+                    key = 2
+                if event.key == pygame.K_3:
+                    key = 3
+                if event.key == pygame.K_4:
+                    key = 4
+                if event.key == pygame.K_5:
+                    key = 5
+                if event.key == pygame.K_6:
+                    key = 6
+                if event.key == pygame.K_7:
+                    key = 7
+                if event.key == pygame.K_8:
+                    key = 8
+                if event.key == pygame.K_9:
+                    key = 9
+
+                if event.key == pygame.K_SPACE:
+                   
+main()
+pygame.quit()
