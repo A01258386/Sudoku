87023c2d93f596fb85895b9b750e31620f163c1c
Gokce Gokmen <51439620+A01258386@users.noreply.github.com>
2021-11-20 17:19:27 -0800

    clavier

--- sudoku.py
+++ sudoku.py
@@ -294,7 +294,25 @@ def main():
                     key = 8
                 if event.key == pygame.K_9:
                     key = 9
-                    
+                #clavier
+                if event.key == pygame.K_KP1:
+                    key = 1
+                if event.key == pygame.K_KP2:
+                    key = 2
+                if event.key == pygame.K_KP3:
+                    key = 3
+                if event.key == pygame.K_KP4:
+                    key = 4
+                if event.key == pygame.K_KP5:
+                    key = 5
+                if event.key == pygame.K_KP6:
+                    key = 6
+                if event.key == pygame.K_KP7:
+                    key = 7
+                if event.key == pygame.K_KP8:
+                    key = 8
+                if event.key == pygame.K_KP9:
+                    key = 9
                 if event.key == pygame.K_DELETE:
                     board.clear()
                     key = None
@@ -314,7 +332,7 @@ def main():
 
                         if board.is_finished():
                             print("Game over")
-
+            #mouvements de la souris
             if event.type == pygame.MOUSEBUTTONDOWN:
                 pos = pygame.mouse.get_pos()
                 clicked = board.click(pos)
